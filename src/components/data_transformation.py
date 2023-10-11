99de0e05ccc5f462d04958f77b5b30cebd6ff190
Elfirasi <elfirasi_oussama@emsi-edu.ma>
2023-10-12 00:21:02 +0100

    1st data transformation update

---
+++ src/components/data_transformation.py
@@ -0,0 +1,9 @@
+import sys
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
